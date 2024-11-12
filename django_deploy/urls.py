235ccb49407ac632d0a4a1968b7e6f1ace359136
shivgopal <39775114+shivgopal@users.noreply.github.com>
2024-11-12 17:16:50 +0530

    Update urls.py

--- django_deploy/urls.py
+++ django_deploy/urls.py
@@ -1,4 +1,4 @@
-"""django_deploy URL Configuration
+"""django_deploy URLd Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
